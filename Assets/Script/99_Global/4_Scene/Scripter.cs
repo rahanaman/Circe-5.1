1636bbb4bea0e74584bcb093eb7a3e50a1ae17c0
aman <gr030315@gmail.com>
2023-07-23 14:58:49 +0900

    Scripter 수정

--- Assets/Script/99_Global/4_Scene/Scripter.cs
+++ Assets/Script/99_Global/4_Scene/Scripter.cs
@@ -1,31 +1,32 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Scripter: MonoBehaviour    //Text를 가지고 있는 GamgObject에 넣어두기
 {
 
-    [SerializeField] private bool _hasParent = true; // 상위 객체도 다시 업로드를 해야하는지 (배경이 있는 경우)
+    
     private RectTransform _parentRect;
     private RectTransform _rect;
     private Text _text;
+    private float _yDelta;
 
-    private void Start()
+    private void Awake()
     {
-        if( _hasParent)
-        {
-            _parentRect = gameObject.transform.parent.GetComponent<RectTransform>();
-        }
-        _rect = gameObject.GetComponent<RectTransform>();
-        _text = gameObject.GetComponent<Text>();
+        _parentRect = GetComponent<RectTransform>();
+        _rect = gameObject.transform.GetChild(0).GetComponent<RectTransform>();
+        _text = gameObject.transform.GetChild(0).GetComponent<Text>();
+        _yDelta = Math.Abs(gameObject.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition.y);
     }
     public void Script(string script) //스크립트 String을 받아와 content 크기에 맞게 조정하는 메소드
     {
+
         _text.text = script;
         LayoutRebuilder.ForceRebuildLayoutImmediate(_rect);
-        _parentRect.sizeDelta = new Vector2(_parentRect.rect.width, _rect.rect.height);
+        _parentRect.sizeDelta = new Vector2(_parentRect.rect.width, _rect.rect.height+2*_yDelta); //
         LayoutRebuilder.ForceRebuildLayoutImmediate(_parentRect);
         //LayoutRebuilder.ForceRebuildLayoutImmediate(content); 사용하면 된대요!
     }
--- Assets/Script/99_Global/debugger.cs
+++ Assets/Script/99_Global/debugger.cs
@@ -7,20 +7,25 @@ using UnityEngine.UI;
 public class debugger : MonoBehaviour
 {
     string[] strings = new string[] { "absdfda\r\ndafllkjlkasdjfljasdlkfjsdlfjlskdjflsajdlfjldajfl\r\ndfjlakdjflsajfldjalfjldsakjflsdjlfjslfkjsdlkfj" };
-    [SerializeField] GameObject text;
+    [SerializeField] GameObject sub;
+    [SerializeField] GameObject SubCon;
     [SerializeField] RectTransform image;
     [SerializeField] Scripter scripter;
     void Start()
     {
-        scripter.Script(strings[0]);
+        GameObject ob = Instantiate(sub, SubCon.transform);
+        Instantiate(sub, SubCon.transform);
+        ob.GetComponent<Scripter>().Script("absdfda\r\ndafllkjlkasdjfljasdlkfjsdlfjlskdjflsajdlfjldajfl\r\ndfjlakdjflsajflddfadfd\n\nfgfdifhkajsdfjkhfkjhdkhskfhakdjhfkjznjalfjldsakjflsdjlfjslfkjsdlkfj");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
     }
 
+    /*
     private void a()
     {
         text.GetComponent<Text>().text = strings[0];
@@ -28,4 +33,5 @@ public class debugger : MonoBehaviour
         image.sizeDelta = new Vector2(image.rect.width, text.GetComponent<RectTransform>().rect.height);
         LayoutRebuilder.ForceRebuildLayoutImmediate(image.GetComponent<RectTransform>());
     }
+    */
 }
