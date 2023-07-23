71575f56f98c4d5da24c3604848804c55cc6a066
aman <gr030315@gmail.com>
2023-07-23 15:52:50 +0900

    카드 비율 수정

--- Assets/Script/3_Battle/CardHandController.cs
+++ Assets/Script/3_Battle/CardHandController.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class CardHandController : MonoBehaviour // 카드 패 정렬하는 Hand 오브젝트 관리
 {
-    private GameObject _hand;
-    private void Start()
-    {
+    private Transform _hand;
 
+    [SerializeField] private Vector3 _rotLeft = new Vector3(0,0,40);
+    [SerializeField] private Vector3 _rotRight = new Vector3(0, 0, -40);
+    private Quaternion _quatLeft;
+    private Quaternion _quatRight;
+    private List<CardView> _cards = new List<CardView>();
+
+    private void Awake()
+    {
+        _hand = gameObject.GetComponent<Transform>();
+        _quatLeft = Quaternion.Euler(_rotLeft);
+        _quatRight = Quaternion.Euler(_rotRight);
+    }
+
+    private void Init()
+    {
+        _cards = new List<CardView>();
     }
 }
--- Assets/Script/99_Global/debugger.cs
+++ Assets/Script/99_Global/debugger.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 
 public class debugger : MonoBehaviour
 {
-    string[] strings = new string[] { "absdfda\r\ndafllkjlkasdjfljasdlkfjsdlfjlskdjflsajdlfjldajfl\r\ndfjlakdjflsajfldjalfjldsakjflsdjlfjslfkjsdlkfj" };
-    [SerializeField] GameObject sub;
+
+    /*
+     * [SerializeField] GameObject sub;
     [SerializeField] GameObject SubCon;
     [SerializeField] RectTransform image;
     [SerializeField] Scripter scripter;
+     */
+    string[] strings = new string[] { "absdfda\r\ndafllkjlkasdjfljasdlkfjsdlfjlskdjflsajdlfjldajfl\r\ndfjlakdjflsajfldjalfjldsakjflsdjlfjslfkjsdlkfj" };
+
+    [SerializeField] CardController _card;
     void Start()
     {
-        GameObject ob = Instantiate(sub, SubCon.transform);
-        Instantiate(sub, SubCon.transform);
-        ob.GetComponent<Scripter>().Script("absdfda\r\ndafllkjlkasdjfljasdlkfjsdlfjlskdjflsajdlfjldajfl\r\ndfjlakdjflsajflddfadfd\n\nfgfdifhkajsdfjkhfkjhdkhskfhakdjhfkjznjalfjldsakjflsdjlfjslfkjsdlkfj");
+       
     }
 
     // Update is called once per frame
@@ -26,12 +29,20 @@ public class debugger : MonoBehaviour
     }
 
     /*
-    private void a()
+    private void 스크린터디버깅()
     {
         text.GetComponent<Text>().text = strings[0];
         LayoutRebuilder.ForceRebuildLayoutImmediate(text.GetComponent<RectTransform>());
         image.sizeDelta = new Vector2(image.rect.width, text.GetComponent<RectTransform>().rect.height);
         LayoutRebuilder.ForceRebuildLayoutImmediate(image.GetComponent<RectTransform>());
+    }
+    */
+    /*
+    private void 스크립터수정()
+    {
+        GameObject ob = Instantiate(sub, SubCon.transform);
+        Instantiate(sub, SubCon.transform);
+        ob.GetComponent<Scripter>().Script("absdfda\r\ndafllkjlkasdjfljasdlkfjsdlfjlskdjflsajdlfjldajfl\r\ndfjlakdjflsajflddfadfd\n\nfgfdifhkajsdfjkhfkjhdkhskfhakdjhfkjznjalfjldsakjflsdjlfjslfkjsdlkfj");
     }
     */
 }
